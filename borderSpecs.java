463bd9e9474fafedd89c36b8e43e40bd1fc6fb9c
Raymond-69 <ray568700362@gmail.com>
2022-01-25 16:58:11 -0500

    IM BAD

--- borderSpecs.java
+++
@@ -1,9 +0,0 @@
-package etf;
-
-public class borderSpecs {
-	//Border Properties
-	public final static int TOP = 100;
-	public final static int BOTTOM = 650;
-	public final static int LEFT = 100;
-	public final static int RIGHT = 950;
-}
